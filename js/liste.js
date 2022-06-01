675d396a297353f51554407373ce79607eed3b95
Bertug <bertugturkeru@msn.com>
2022-06-01 03:14:44 +0300

    localstorage responsivity

--- js/liste.js
+++ js/liste.js
@@ -1,16 +1,18 @@
 // Identifies some DOM elements
-  const
+  let
     input = document.getElementById("task"),
     newList = document.getElementById("list"),
     items = document.getElementsByClassName('list-group-item'),
-    button = document.getElementById("liveToastBtn");
+    button = document.getElementById("liveToastBtn"),
     ayn = document.querySelector('#samesame')
+    
   // Focuses input, and calls addItem on button-click
   input.focus();
   button.addEventListener('click', addItem);
 
   // Defines the listener function
   function addItem() {
+     
     // Trims whitespace and sets string to lowerCase
     const inputTrimmedLower = input.value.trim().toLocaleLowerCase();
     
@@ -26,7 +28,7 @@
       const liTrimmedLower = li.firstChild.nodeValue
       if (liTrimmedLower === inputTrimmedLower) {
          ayn.innerHTML = `${inputTrimmedLower} is already listed`
-          console.log(liTrimmedLower)
+          
          toast1();
         return;
       }
@@ -44,18 +46,18 @@
     newItem.append(closeBtn)
     closeBtn.onclick = removeListItem
     newItem.onclick = checked
-  }
+    saveAll()
+}
+
 
   function checked() {
     this.classList.toggle("checked")
-  }
-
- 
-  
+}  
   function removeListItem() {
     this.parentElement.remove();
-  }
-
+    localStorage.removeItem('elemanlar')
+    saveAll()
+}
 
 
 function toast0() {
@@ -64,18 +66,35 @@ function toast0() {
 
    $('#liveToast0').toast('show');
 }
-
 function toast1() {
    $('#liveToast0').toast('hide');
    $('#liveToast').toast('hide');
 
    $('#liveToast1').toast('show');
 }
-
-
 function toast2() {
   $('#liveToast0').toast('hide')
   $('#liveToast1').toast('hide')
 
   $('#liveToast').toast('show')
 }
+
+
+function saveAll() {
+    var toStorage = [];
+    for (var i = 0; i < items.length; i++) {
+      toStorage.push(items[i].firstChild.nodeValue)
+      localStorage.clear('elemanlar')
+      localStorage.setItem('elemanlar', JSON.stringify(toStorage))
+    }
+}
+
+function loadAll() {
+  const storedvalue = JSON.parse(localStorage.getItem('elemanlar'))
+    for (var i = 0; i < storedvalue.length; i++) {
+      input.value = storedvalue[i]
+      addItem();
+    }   
+}
+  
+loadAll();
