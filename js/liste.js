3f992e7038378633ab63a30c55d22c5ef64cc7e1
Bertug <bertugturkeru@msn.com>
2022-06-02 17:33:50 +0300

    done

--- js/liste.js
+++ js/liste.js
@@ -7,29 +7,29 @@
     ayn = document.querySelector('#samesame')
     
   // Focuses input and calls addItem on button-click
-  input.focus();
-  button.addEventListener('click', addItem);
+  input.focus()
+  button.addEventListener('click', addItem)
 
   // Defines the listener function
   function addItem() {
      
     // Trims whitespace and sets string to lowerCase
-    const inputTrimmedLower = input.value.trim().toLocaleLowerCase();
+    const inputTrimmedLower = input.value.trim().toLocaleLowerCase()
     
     // Clears and refocuses input
-    input.value = "";
-    input.focus();
+    input.value = ""
+    input.focus()
     
    // Not to add empty input
-    if (!inputTrimmedLower) { return  toast0(); }
+    if (!inputTrimmedLower) { return  toast0() }
     
     // Not to add value if a list item matches it
     for (const li of items) {
       const liTrimmedLower = li.firstChild.nodeValue // to avoid the span we added with the x button
       if (liTrimmedLower === inputTrimmedLower) {
          ayn.innerHTML = `${inputTrimmedLower} is already listed`
-        toast1();
-        return;
+        toast1()
+        return
       }
     }
 
@@ -54,23 +54,23 @@
 }
   // Remove list element when clicked on closeBtn and first removes then add the rest of the list elements to localstore by calling saveAll
   function removeListItem() {
-    this.parentElement.remove();
+    this.parentElement.remove()
     localStorage.removeItem('elemanlar')
     saveAll()
 }
 
 // Hide all the other toasts first to avoid any conflict
 function toast0() {
-   $('#liveToast').toast('hide');
-   $('#liveToast1').toast('hide');
+   $('#liveToast').toast('hide')
+   $('#liveToast1').toast('hide')
 
-   $('#liveToast0').toast('show');
+   $('#liveToast0').toast('show')
 }
 function toast1() { 
-   $('#liveToast0').toast('hide');
-   $('#liveToast').toast('hide');
+   $('#liveToast0').toast('hide')
+   $('#liveToast').toast('hide')
 
-   $('#liveToast1').toast('show');
+   $('#liveToast1').toast('show')
 }
 function toast2() {
   $('#liveToast0').toast('hide')
@@ -81,20 +81,20 @@ function toast2() {
 
 // LocalStorage.setItem
 function saveAll() {
-    var toStorage = []; // Create an empty array
+    var toStorage = [] // Create an empty array
     for (var i = 0; i < items.length; i++) {
       toStorage.push(items[i].firstChild.nodeValue) // Pushing every list element's values into our array, avoiding span elements
       localStorage.clear('elemanlar')
       localStorage.setItem('elemanlar', JSON.stringify(toStorage)) // Store array with the key name "elemanlar" 
     }
 }
-
+// LocalStorage.getItem
 function loadAll() {
-  const storedvalue = JSON.parse(localStorage.getItem('elemanlar'))
+  let storedvalue = JSON.parse(localStorage.getItem('elemanlar'))
     for (var i = 0; i < storedvalue.length; i++) {
       input.value = storedvalue[i] // Get every value of the array, make them input values and call addItem to add them like the first time
-      addItem();
+      addItem()
     }   
 }
-  
+// Checking 
 loadAll();
